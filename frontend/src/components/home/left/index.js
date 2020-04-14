d17f70daab9380281aa75799381f0ce90c8e76c6
migom6 <migom6@gmail.com>
2020-04-14 15:20:41 +0530

    delete cra

--- frontend/src/components/home/left/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const left = () => <div id="left">LEft</div>
-
-export default left;
--- frontend/src/components/home/right/Event.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Event = ({ title, text, event_date }) =>
-    (<div className="event">
-        {title}
-    </div>);
-
-
-export default Event;
