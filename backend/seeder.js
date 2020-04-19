ff79e53ae4284273f5bf6d24d9e7890d3ec8400e
somveer <iit2015009@iiita.ac.in>
2020-04-19 19:51:32 +0530

    seeder file added

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,72 +1,131 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const colors = require('colors');
-const dotenv = require('dotenv');
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const asyncHandler = require("./middleware/async");
+const connectDB = require("./config/db");
 
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Load models
-const Bootcamp = require('./models/Bootcamp');
-const Course = require('./models/Course');
-const User = require('./models/User');
-const Review = require('./models/Review');
+const User = require("./models/User");
+const Feed = require("./models/Feed");
+const Comment = require("./models/Comment");
+const Post = require("./models/Post");
+const Poll = require("./models/Poll");
+const Event = require("./models/Event");
+const EventUser = require("./models/EventUser");
+const PollUser = require("./models/Post");
+const Like = require("./models/Like");
+const Dislike = require("./models/Dislike");
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
+var con = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+});
+
+mongoose.connect(process.env.MONGO_URI, function () {
+  /* Drop the DB */
+  mongoose.connection.db.dropDatabase();
 });
 
 // Read JSON files
-const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+const usersFile = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
+const postsFile = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/posts.json`, "utf-8")
+);
+const eventsFile = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/events.json`, "utf-8")
+);
+const pollsFile = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/polls.json`, "utf-8")
+);
+const commentsFile = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/comments.json`, "utf-8")
+);
+const feedsFile = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/feeds.json`, "utf-8")
 );
 
-const courses = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
-);
+// local store the variables
 
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
-);
+var users = [];
 
-const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
-);
+const createDb = asyncHandler(async () => {
+  // Create Users and store them in users array
+  await Promise.all(
+    usersFile.map(async (user_data, index) => {
+      user = await User.create(user_data);
+      users.push(user);
+    })
+  );
 
-// Import into DB
-const importData = async () => {
-  try {
-    await Bootcamp.create(bootcamps);
-    await Course.create(courses);
-    await User.create(users);
-    await Review.create(reviews);
-    console.log('Data Imported...'.green.inverse);
-    process.exit();
-  } catch (err) {
-    console.error(err);
-  }
-};
+  let userId = users[0]._id;
+  let postId = 0;
+  let eventId = 0;
+  let pollId = 0;
+  let feedId = 0;
+  let commentId = 0;
 
-// Delete data
-const deleteData = async () => {
-  try {
-    await Bootcamp.deleteMany();
-    await Course.deleteMany();
-    await User.deleteMany();
-    await Review.deleteMany();
-    console.log('Data Destroyed...'.red.inverse);
-    process.exit();
-  } catch (err) {
-    console.error(err);
-  }
-};
+  //create the feeds
+  await Promise.all(
+    feedsFile.map(async (feed) => {
+      let feedIdLocal;
 
-if (process.argv[2] === '-i') {
-  importData();
-} else if (process.argv[2] === '-d') {
-  deleteData();
-}
+      if (feed.kind === "post") {
+        await Promise.all(
+          postsFile.map(async (post_data) => {
+            // first making the post then create the feed
+            post = await Post.create(post_data);
+            feed = await Feed.create({ user: userId, post, kind: feed.kind });
+            if (feedId === 0 && postId === 0) {
+              postId = post._id;
+              feedId = feed._id;
+            }
+            feedIdLocal = feed._id;
+          })
+        );
+      } else if (feed.kind === "event") {
+        await Promise.all(
+          eventsFile.map(async (event_data) => {
+            event = await Event.create(event_data);
+            feed = await Feed.create({ user: userId, event, kind: feed.kind });
+            if (eventId === 0) {
+              eventId = event._id;
+            }
+            feedIdLocal = feed._id;
+          })
+        );
+      } else if (feed.kind === "poll") {
+        await Promise.all(
+          pollsFile.map(async (poll_data) => {
+            poll = await Poll.create(poll_data);
+            feed = await Feed.create({ user: userId, poll, kind: feed.kind });
+            if (pollId === 0) {
+              pollId = poll._id;
+            }
+            feedIdLocal = feed._id;
+          })
+        );
+      }
+
+      await Promise.all(
+        commentsFile.map(async (comment_data, index) => {
+          comment = await Comment.create({
+            text: comment_data.text,
+            feed: feedIdLocal,
+            user: users[index + 1]._id,
+          });
+        })
+      );
+    })
+  );
+});
+
+createDb();
